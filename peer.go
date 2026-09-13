e1c10e8cc2f394639d8bc770b3b99b19a5ac3731
Cian Flint <100323647+CianFlint@users.noreply.github.com>
2026-09-13 03:08:27 +0100

    removed test and bytesfast

--- peer.go
+++ peer.go
@@ -104,25 +104,6 @@ func (peer enetPeer) SendBytes(data []byte, channel uint8, flags PacketFlags) er
 	return peer.SendPacket(packet, channel)
 }
 
-func (peer enetPeer) SendBytesFast(data []byte, channel uint8, isReliable bool) error {
-	var flags uint32 = 0
-	if isReliable {
-		flags = 1
-	}
-
-	dataPtr := unsafe.Pointer(&data[0])
-	dataLen := C.size_t(len(data))
-
-	C.enet_peer_send_fast(
-		peer.cPeer,
-		(C.enet_uint8)(channel),
-		dataPtr,
-		dataLen,
-		(C.enet_uint32)(flags),
-	)
-	return nil
-}
-
 func (peer enetPeer) SendString(str string, channel uint8, flags PacketFlags) error {
 	packet, err := NewPacket([]byte(str), flags)
 	if err != nil {
@@ -147,11 +128,6 @@ func (peer enetPeer) RelayPacket(packet Packet, channel uint8) error {
 		(C.enet_uint8)(channel),
 		packet.(enetPacket).cPacket,
 	)
-	return nil
-}
-
-func (peer enetPeer) MinimalTest(value int) error {
-	C.enet_test_minimal((C.int)(value))
 	return nil
 }
 
