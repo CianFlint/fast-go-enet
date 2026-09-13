e0da3342388196b187c083d8b3c3eea250706dc0
Cian Flint <cianflint@gmail.com>
2026-09-13 01:55:08 +0100

    undo

--- enetc.go
+++ enetc.go
@@ -1,6 +1,6 @@
 package enet
 
-// #cgo !windows LDFLAGS: -lm -pthread
+// #cgo !windows pkg-config: libenet
 // #cgo windows CFLAGS: -Ienet/include/
 // #cgo windows LDFLAGS: -Lenet/ -lenet -lws2_32 -lwinmm
 // #include <enet/enet.h>
