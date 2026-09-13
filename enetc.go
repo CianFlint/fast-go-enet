97972f02c03fe4dcccd93401e02ebdea245a2537
Cian Flint <100323647+CianFlint@users.noreply.github.com>
2026-09-13 06:18:34 +0100

    CFLAGS: -O3

--- enetc.go
+++ enetc.go
@@ -1,7 +1,7 @@
 package enet
 
 // #cgo !windows pkg-config: libenet
-// #cgo windows CFLAGS: -Ienet/include/
+// #cgo windows CFLAGS: -O3 -fomit-frame-pointer -Ienet/include/
 // #cgo windows LDFLAGS: -Lenet/ -lenet -lws2_32 -lwinmm
 // #include <enet/enet.h>
 import "C"
